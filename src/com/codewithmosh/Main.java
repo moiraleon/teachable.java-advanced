f1228b8abfa5c124805a2c7b1ea6aacf49598183
Moira Leon <moira.leon2@gmail.com>
2021-10-30 12:20:37 -0600

    Generics

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,7 +1,9 @@
 package com.codewithmosh;
 
+import com.codewithmosh.generics.GenericList;
+
 public class Main {
     public static void main(String[] args) {
-
+    new GenericList<Integer>().add(); //generic lists ensure that every object in the list is the correct types
     }
 }
