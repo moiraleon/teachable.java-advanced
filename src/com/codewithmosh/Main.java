718d22be867fdeace5520b8598ef7b8d0e8a99ce
Moira Leon <moira.leon2@gmail.com>
2021-11-01 17:15:16 -0600

    Generic Lists

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,9 +1,15 @@
 package com.codewithmosh;
 
 import com.codewithmosh.generics.GenericList;
+import com.codewithmosh.generics.User;
 
 public class Main {
     public static void main(String[] args) {
-    new GenericList<Integer>().add(); //generic lists ensure that every object in the list is the correct types
+   // new GenericList<Integer>().add(); //generic lists ensure that every object in the list is the correct types
+        var list  = new GenericList<User>();
+        list.add(new User());
+
+        User user = list.get(0); //generic lists help us to avoid casting and create cleaner code and check for errors at compile time not run time
+
     }
 }
