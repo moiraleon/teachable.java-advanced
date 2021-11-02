f003e38017da5d8622929b09378e22588dffa1d3
Moira Leon <moira.leon2@gmail.com>
2021-11-02 12:57:03 -0600

    functional interfaces

--- src/com/codewithmosh/lambdas/ConsolePrinter.java
+++ src/com/codewithmosh/lambdas/ConsolePrinter.java
@@ -1,6 +1,10 @@
 package com.codewithmosh.lambdas;
 
 public class ConsolePrinter implements Printer {
+//  @Override
+//  public void print(String message) { //hovering over implementation of the interface will allow for implement method option
+//
+//  }
   @Override
   public void print(String message) {
     System.out.println(message);
