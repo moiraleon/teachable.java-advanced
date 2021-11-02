55526c73799ca43ed732553f5d1a35771a99fbfe
Moira Leon <moira.leon2@gmail.com>
2021-11-02 16:05:26 -0600

    funcition interface in java

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) { //if we use extends + Interface i.e. Number than the list items can only be derived from the number class
    // new GenericList<Integer>().add(); //generic lists ensure that every object in the list is the correct types
         var list  = new GenericList<User>();
-        list.add(new User());
+//        list.add(new User());
 
         User user = list.get(0); //generic lists help us to avoid casting and create cleaner code and check for errors at compile time not run time
 
--- src/com/codewithmosh/lambdas/LambdasDemo.java
+++ src/com/codewithmosh/lambdas/LambdasDemo.java
@@ -1,9 +1,15 @@
 package com.codewithmosh.lambdas;
 
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 public class LambdasDemo {
   public static void show() { //implementing  a method and overrid within a method is considered an anonymous class
+    Function<String,Integer> map =  str -> str.length();
+    var length = map.apply("Sky");
+    System.out.println(length);
+
+
     UnaryOperator<Integer> square = n -> n * n;
     UnaryOperator<Integer> increment = n -> n + 1;
 
@@ -14,14 +20,14 @@ public class LambdasDemo {
     //greet(message ->{ //in addition if there is only one line of code curly braces are not needed
       //System.out.println(message)
     //});  //LAMBDA EXPRESSION uses arrow function notation
-    greet(message -> System.out.println(message)); //simple lambda expression
-
-    Printer printer = message -> System.out.println(message);//lambda expressions are essentially objects and can be saved as variables -- they can be used to represent anonymous funcitons
-
-    greet(System.out::println); //method reference of the above lambda expression- method references allow us to write compact and easier to read lambda expressions
-    }
-
-  public static void greet(Printer printer){
-    printer.print("Hello World");
+//    greet(message -> System.out.println(message)); //simple lambda expression
+//
+//    Printer printer = message -> System.out.println(message);//lambda expressions are essentially objects and can be saved as variables -- they can be used to represent anonymous funcitons
+//
+//    greet(System.out::println); //method reference of the above lambda expression- method references allow us to write compact and easier to read lambda expressions
+//    }
+//
+//  public static void greet(Printer printer){
+//    printer.print("Hello World");
   }
 }
