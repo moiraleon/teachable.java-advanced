86b83361ab7327e33e79d5936bedeeea88d341f0
Moira Leon <moira.leon2@gmail.com>
2021-11-02 12:53:11 -0600

    functional interfaces

--- src/com/codewithmosh/lambdas/Printer.java
+++ src/com/codewithmosh/lambdas/Printer.java
@@ -1,5 +1,5 @@
 package com.codewithmosh.lambdas;
 
 public interface Printer {
-  void print(String message);
+  void print(String message);  //this interface is functional because it contains a single abstract message -- we can write code that can be implemented here but it is not considered best practice
 }
