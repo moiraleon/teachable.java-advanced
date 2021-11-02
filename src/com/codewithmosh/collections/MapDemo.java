602e55f8f4e65599365948d23f80ccd3f3725ffa
Moira Leon <moira.leon2@gmail.com>
2021-11-01 21:53:02 -0600

    Map interfacing in Java

--- src/com/codewithmosh/collections/MapDemo.java
+++ src/com/codewithmosh/collections/MapDemo.java
@@ -12,21 +12,24 @@ public class MapDemo {
     map.put(c1.getEmail(), c1);
     map.put(c2.getEmail(), c2);
 
-    var exists = map.containsKey("e1");
+    var exists = map.containsKey("e1"); //returns true or false
 
     var unknown = new Customer("Unknown", "");
-    var customer = map.get("e1");
-    customer = map.getOrDefault("e1", unknown);
+    var customer = map.get("e1"); //instead of using a loop retrieves the name of the customer with this email value --cost is low to do this search
+    customer = map.getOrDefault("e1", unknown);//if not found passes unknown customer
 
-    map.replace("e1", new Customer("a++", "e1"));
+    map.replace("e1", new Customer("a++", "e1"));//like update in sql - provide current reference point and replace value to be input
 
     for (var key : map.keySet())
       System.out.println(key);
 
-    for (var value : map.values())
+    for (var value : map.values()) //returns a collection of customers //also sets never guarantee order
       System.out.println(value);
 
     for (var entry : map.entrySet())
       System.out.println(entry);
+    //can supply entry.getKey ot entry.getValue
   }
 }
+//hash tables are represented as O(1) they are called dictionaries in python and c# and objects in javascript
+//called a map because it maps a key to its value and a dictionary because it matches a words to its meaning
