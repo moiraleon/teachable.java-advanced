138e02271ed019c591b45b4f6adfe490ecfeb4ef
Moira Leon <moira.leon2@gmail.com>
2021-11-03 13:47:41 -0600

    interrupting a thread

--- src/com/codewithmosh/concurrency/DownloadFileTask.java
+++ src/com/codewithmosh/concurrency/DownloadFileTask.java
@@ -13,7 +13,7 @@ public class DownloadFileTask implements Runnable {
     System.out.println("Downloading a file: " + Thread.currentThread().getName());
 
     for (var i = 0; i < 10_000; i++) {
-      if (Thread.currentThread().isInterrupted()) return;
+      if (Thread.currentThread().isInterrupted()) return; //interrupts simply send an interrupt request, it is up to the thread to select whether to be interrupted or not, else we should code it to listen for interruptions
       status.incrementTotalBytes();
     }
 
