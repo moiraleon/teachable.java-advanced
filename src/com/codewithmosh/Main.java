bff52db5e9dfb43bc86a9abc3424f3dab02429f7
Moira Leon <moira.leon2@gmail.com>
2021-11-03 13:33:49 -0600

    Processors and Threads

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,12 +4,27 @@ import com.codewithmosh.generics.GenericList;
 import com.codewithmosh.generics.User;
 
 public class Main {
-    public static void main(String[] args) { //if we use extends + Interface i.e. Number than the list items can only be derived from the number class
-   // new GenericList<Integer>().add(); //generic lists ensure that every object in the list is the correct types
-        var list  = new GenericList<User>();
-//        list.add(new User());
+    public static void main(String[] args) {
 
-        User user = list.get(0); //generic lists help us to avoid casting and create cleaner code and check for errors at compile time not run time
 
+        System.out.println(Thread.activeCount());
+        System.out.println(Runtime.getRuntime().availableProcessors());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+//if we use extends + Interface i.e. Number than the list items can only be derived from the number class
+// new GenericList<Integer>().add(); //generic lists ensure that every object in the list is the correct types
+//   var list  = new GenericList<User>();
+//        list.add(new User());
+
+//   User user = list.get(0); //generic lists help us to avoid casting and create cleaner code and check for errors at compile time not run time
