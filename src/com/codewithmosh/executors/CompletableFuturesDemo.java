436f6aa5b56af6c9603999d89d0aa7f44173d6e8
Moira Leon <moira.leon2@gmail.com>
2021-11-04 16:50:26 -0600

    transforming a completable future

--- src/com/codewithmosh/executors/CompletableFuturesDemo.java
+++ src/com/codewithmosh/executors/CompletableFuturesDemo.java
@@ -3,11 +3,31 @@ package com.codewithmosh.executors;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 public class CompletableFuturesDemo {
+    public static int toFahrenheit(int celsius){
+        return (int) (celsius * 1.8)+32;
+    }
   public static void show() {
-    var start = LocalTime.now();
+      var future1  = CompletableFuture.supplyAsync(()->20);
+        future1
+              .thenApply(CompletableFuturesDemo::toFahrenheit )
+              .thenAccept(f -> System.out.println(f));
+ //      try {  //Complete futures allow us to create complex asynchronous functions in a declarative way
+//          var result = future1
+//                  .thenApply(CompletableFuturesDemo::toFahrenheit )
+//                  .get();
+//          System.out.println(result);
+//      } catch (InterruptedException e) {
+//          e.printStackTrace();
+//      } catch (ExecutionException e) {
+//          e.printStackTrace();
+//      }
+
+
+      var start = LocalTime.now();
 
     var service = new FlightService();
     var futures = service.getQuotes()
